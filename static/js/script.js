79aec97cb19b3e5e41244647605f9d2fdb4c1d30
Zephyr Prusinski <zephyr@zephnet.biz>
2025-07-11 14:04:48 -0400

    light / dark swapping js

---
+++ static/js/script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const setColorScheme = (schemePref) => {
+    window.localStorage.setItem("schemePref", schemePref);
+    document.documentElement.dataset.schemePref = schemePref;
+  };
+
+  let schemePref = "dark";
+  if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: light)").matches
+  ) {
+    schemePref = "light";
+  }
+  if (document.documentElement.dataset.schemePref) {
+    schemePref = document.documentElement.dataset.schemePref;
+  }
+  const schemePrefToggleButton = document.querySelector(
+    "#schemePrefToggleButton",
+  );
+  if (schemePrefToggleButton) {
+    schemePrefToggleButton.addEventListener("click", (e) => {
+      e.preventDefault();
+      if (schemePref == "dark") {
+        schemePref = "light";
+      } else {
+        schemePref = "dark";
+      }
+      setColorScheme(schemePref);
+    });
+  }
+});
