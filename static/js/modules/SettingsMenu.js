368c67ecc3206242f9c543add4811617bc9c63fe
Zephyr Prusinski <zephyr@zephnet.biz>
2025-11-17 10:57:34 -0500

    updates to the settingsmenu js for more options to close the menu

--- static/js/modules/SettingsMenu.js
+++ static/js/modules/SettingsMenu.js
@@ -8,12 +8,38 @@ class SettingsMenu extends HTMLElement {
     const closeMenuButton = this.querySelector('.closeMenuButton');
     const options = this.querySelector("#options");
 
+    const closeMenu = () => {
+      this.classList.remove("open");
+      this.open = false;
+      gearAnchor.setAttribute('aria-expanded', 'false');
+    }
+
+    const closeMenuIfNotClicked = (e) => {
+      if (e.target.closest('settings-menu') == null){
+        closeMenu();
+      };
+    }
+
+    const closeMenuOnEsc = (e) => {
+      if (e.key == 'Escape') {
+        closeMenu();
+      }
+    }
+
     const menuControlAddEventListener = (element) => {
       element.addEventListener("click", (e) => {
         e.preventDefault();
         this.classList.toggle("open");
         this.open = !this.open;
         gearAnchor.setAttribute('aria-expanded', this.open.toString());
+        if (this.open) {
+          document.addEventListener("click", closeMenuIfNotClicked(e));
+          document.addEventListener("keyup", closeMenuOnEsc(e));
+        }
+        else {
+          document.removeEventListener("click", closeMenuIfNotClicked);
+          document.removeEventListener("keyup", closeMenuOnEsc);
+        }
       });
     }
 
@@ -25,7 +51,6 @@ class SettingsMenu extends HTMLElement {
       // don't let the click events bubble up to the gearAnchor (which would otherwise close the menu)
       e.stopPropagation();
     });
-
   }
 }
 
